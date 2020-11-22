dbd09e72a8171f629f691bace29dd3d179a5f8da
孙昊翔 <673686754@qq.com>
2020-11-22 12:17:53 +0800

    no message

--- checkin_env.js
+++ checkin_env.js
@@ -39,9 +39,6 @@ Modified by evilbutcher
 */
 const $ = new Env("机场签到");
 $.autoLogout = true;
-console.log('*************************************')
-console.log($.getdata("evil_autoLogout"))
-console.log('*************************************')
 
 if (
   $.getdata("evil_checkintitle") != undefined &&
